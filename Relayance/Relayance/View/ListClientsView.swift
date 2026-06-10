c67649aabe66b75dbfbe0341bb155168e9036c01
Mathieu ARRIO <minimat26git@arrio.fr>
2026-06-10 20:34:16 +0200

    feat(view-viewModel): Add AjoutClient and SuppresionCLient feature - Refactor as MVVM Architecture

--- Relayance/Relayance/View/ListClientsView.swift
+++ Relayance/Relayance/View/ListClientsView.swift
@@ -2,20 +2,18 @@
 //  ListClientsView.swift
 //  Relayance
 //
-//  Created by Amandine Cousin on 10/07/2024.
-//
 
 import SwiftUI
 
 struct ListClientsView: View {
-    @State var clientsList: [Client] = ModelData.chargement("Source.json")
-    @State private var showModal: Bool = false
+    @State private var viewModel = ClientViewModel()
+    @State private var showModal = false
 
     var body: some View {
         NavigationStack {
-            List(clientsList, id: \.self) { client in
+            List(viewModel.clientsList, id: \.self) { client in
                 NavigationLink {
-                    DetailClientView(client: client)
+                    DetailClientView(client: client, viewModel: viewModel)
                 } label: {
                     Text(client.nom)
                         .font(.title3)
@@ -31,14 +29,14 @@ struct ListClientsView: View {
                     .bold()
                 }
             }
-            .sheet(isPresented: $showModal, content: {
-                AjoutClientView(dismissModal: $showModal)
-            })
+            .sheet(isPresented: $showModal) {
+                AjoutClientView(viewModel: viewModel, dismissModal: $showModal)
+            }
         }
     }
-
 }
 
 #Preview {
     ListClientsView()
 }
+
